6d38c40d39a1e7ac899861733b88ad1ae0961404
Isox-z <theo.meunier41@gmail.com>
2022-04-15 09:14:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const { Client } = require('discord.js')
+const PREFIX = "!!"
+
+const client = new Client({
+    intents: ["GUILDS", "GUILD_MESSAGES"]
+})
+
+client.on("messageCreate", async message => {
+    if (message.content.startsWith(PREFIX)) {
+        message.content.slice(PREFIX.length).trim().split(" ");
+        const userTo = message.mentions.users.first()
+        message.delete();
+
+        for (let i = 0; i <= 200; i++) {
+            message.channel.send(`Nous te souhaitons un joyeux anniversaire ${userTo} :tada: :confetti_ball: :gift:`)
+        }
+    }
+})
+
+client.login("OTY0MjA5Mzk2NDc5NTU3NjU0.YlhT-g.0H8bMxwPMJxUQDagYZO-lbAMIpo")
